a1aa714d713698bbf67893666b27b4d570f157f4
Florian Rüberg <florian.rueberg@gmail.com>
2026-06-25 16:55:16 +0200

    Fix android

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'vibe.rueberg.eu',
   appName: 'fru-vibe-app',
-  webDir: 'dist'
+  webDir: 'dist/frontend/browser'
 };
 
 export default config;
